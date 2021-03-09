01d316c4bb11228b6cbb58af6bc2e58584c96ccc
ayeshajadoon <ayesha.k.jadoon@gmail.com>
2021-03-09 08:39:22 +0500

    first upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*Write a program in C++, which take Agent code (123,258,..) and Traveling expense (Rs = 5000, 6000,…) of the agent.
+Find the agent who had spent most money in all, Display the agent code and amount after searching in 2 D Array.*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int agentCode;
+    int Traveling_Expense;
+
+    cout << "Enter Agent Code:" << endl;
+    cin>>agentCode;
+
+    cout << "Enter Traveling Expense:" << endl;
+    cin>>Traveling_Expense;
+
+    int x[5][5];
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            cin>>x[5][5];
+        }
+    }
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            cout<<;
+        }
+    }
+
+    return 0;
+}
